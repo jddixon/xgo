bd523595a43c3cb30bc0bb3fbdc61093316c4a9e
Jim Dixon <jddixon@gmail.com>
2014-03-16 15:44:08 -0700

    adding parseEndTag.go

--- xml/xmlpull/parseEndTag.go
+++ xml/xmlpull/parseEndTag.go
@@ -6,11 +6,56 @@ import (
 
 var _ = fmt.Print
 
-// [42] ETag ::=  '</' Name S? '>'
-//
 func (p *Parser) parseEndTag() (curEvent PullEvent, err error) {
 
-	// XXX STUB
+	//ASSUMPTION ch is past "</"
 
+	// [42] ETag ::=  '</' Name S? '>'
+
+	var startName, endName []rune
+
+	ch, err := p.NextCh()
+	if err == nil && isNameStartChar(ch) {
+		err = p.NewXmlPullError(
+			"expected name start and not " + printableChar(ch))
+		for err == nil {
+			ch, err = p.NextCh()
+			if !isNameChar(ch) {
+				break
+			}
+			endName = append(endName, ch)
+		}
+	}
+	if err == nil {
+		// we have endName; ch must be positioned at either S or >
+		startName = p.elName[p.elmDepth]
+		if !SameRunes(endName, startName) {
+			msg := fmt.Sprintf(
+				"line %d: start tag %s differs from end tag %s\n",
+				p.elRawNameLine[p.elmDepth], startName, endName)
+			err = p.NewXmlPullError(msg)
+		}
+	}
+	if err == nil {
+		// skip optional white space
+		p.SkipS()
+		// require a closing right angle bracket
+		ch, err = p.NextCh()
+		if err == nil {
+			if ch != '>' {
+				msg := fmt.Sprintf(
+					"line %d: expected > to finish end tag not %x",
+					p.elRawNameLine[p.elmDepth], printableChar(ch))
+				err = p.NewXmlPullError(msg)
+			}
+		}
+	}
+	if err == nil {
+		//namespaceEnd = elNamespaceCount[ p.elmDepth ]; //FIXME
+		p.elmDepth-- // XXX NEEDS TESTING
+		p.pastEndTag = true
+		curEvent = END_TAG
+		p.curEvent = curEvent
+	}
 	return
 }
