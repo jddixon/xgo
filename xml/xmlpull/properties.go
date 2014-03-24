fba21ebb21f2fe8343fed945109de8b47b4eda6c
Jim Dixon <jddixon@gmail.com>
2014-03-23 18:39:18 -0700

    cleanup of parser*.go

--- xml/xmlpull/properties.go
+++ xml/xmlpull/properties.go
@@ -7,28 +7,28 @@ func MakeCopyRunes(src []rune) (dest []rune, err error) {
 		err = EmptyRuneSlice
 	} else {
 		dest = make([]rune, len(src))
-		copy (dest, src)
+		copy(dest, src)
 	}
 	return
 }
 
-// Return a copy of the tag name (in which case the argument is nil 
+// Return a copy of the tag name (in which case the argument is nil
 // or empty, or copy the parameter as an entity ref name.
-	
+
 func (p *Parser) getName(candidate []rune) (runes []rune, err error) {
 
-    if p.curEvent == START_TAG {
-        runes, err = MakeCopyRunes( p.elName[ p.elmDepth ] )
-    } else if p.curEvent == END_TAG {
-        runes, err = MakeCopyRunes( p.elName[ p.elmDepth ] )
-    } else if p.curEvent == ENTITY_REF {
-        if p.entityRefName == nil {
+	if p.curEvent == START_TAG {
+		runes, err = MakeCopyRunes(p.elName[p.elmDepth])
+	} else if p.curEvent == END_TAG {
+		runes, err = MakeCopyRunes(p.elName[p.elmDepth])
+	} else if p.curEvent == ENTITY_REF {
+		if p.entityRefName == nil {
 			runes, err = MakeCopyRunes(candidate)
 			if err == nil {
 				p.entityRefName, err = MakeCopyRunes(runes)
 			}
-        }
-    }
+		}
+	}
 	if err != nil {
 		runes = nil
 	}
@@ -37,13 +37,13 @@ func (p *Parser) getName(candidate []rune) (runes []rune, err error) {
 
 func (p *Parser) getNamespace() (runes []rune, err error) {
 
-    if p.curEvent == START_TAG || p.curEvent == END_TAG {
-        if p.processNamespaces {
-			runes, err = MakeCopyRunes(p.elUri[ p.elmDepth  ])
-		} 
+	if p.curEvent == START_TAG || p.curEvent == END_TAG {
+		if p.processNamespaces {
+			runes, err = MakeCopyRunes(p.elUri[p.elmDepth])
+		}
 	}
 	if runes == nil {
 		runes, err = MakeCopyRunes(NO_NAMESPACE)
 	}
-    return 
-} 
+	return
+}
